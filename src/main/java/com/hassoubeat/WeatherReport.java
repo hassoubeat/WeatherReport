5b52c219b75e56c0d6ff13a4b4f7f0ca7987f9df
hassoubeat <hassoubeat.work@gmail.com>
2017-04-23 16:52:36 +0900

    [update] 外部プロパティを参照するように処理を変更
    外部プロパティを参照するように処理を変更
    ・外部プロパティを参照し、動的に天気を喋れるように変更

--- src/main/java/com/hassoubeat/WeatherReport.java
+++ src/main/java/com/hassoubeat/WeatherReport.java
@@ -16,21 +16,38 @@ import javax.ws.rs.core.MediaType;
  */
 public class WeatherReport implements FacetInterface{
     
+    private final String PROPERTIES_FILE_PATH = "WeatherReport.properties"; 
     private String weather_hacks_url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=";
     
     @Override
     public Result execute() throws Exception {
+        // デフォルトは東京のID
+        String cityId = "130010";
+        cityId = PropertyUtil.getInstace().load(PROPERTIES_FILE_PATH, "city.id");
         RestClient restClient = new RestClient();
-        String resultStr = restClient.requestGET(weather_hacks_url + "016010", MediaType.APPLICATION_JSON_TYPE);
+        String resultStr = restClient.requestGET(weather_hacks_url + cityId, MediaType.APPLICATION_JSON_TYPE);
         ObjectMapper mapper = new ObjectMapper();
         WeatherObject jsonObject = mapper.readValue(resultStr, WeatherObject.class);
         String reportTitle = jsonObject.title;
         String toDayAboutReport = jsonObject.forecasts[0].telop;
-        String toDayCelsius = jsonObject.forecasts[0].temperature.max.celsius;
+        String toDayCelsius;
+        try {
+            toDayCelsius = jsonObject.forecasts[0].temperature.max.celsius;
+        } catch (NullPointerException ex) {
+            toDayCelsius = null;
+        }
         String tomorrowAboutReport = jsonObject.forecasts[1].telop;
         String tomorrowMaxCelsius = jsonObject.forecasts[1].temperature.max.celsius;
         String tomorrowMinCelsius = jsonObject.forecasts[1].temperature.min.celsius;
-        String talking = reportTitle + "です。" + "今日の天気と気温です。" + toDayAboutReport + "。温度" + toDayCelsius + "度。明日の天気と気温です。" + tomorrowAboutReport + "。最高温度" + tomorrowMaxCelsius + "度。" + "最低温度" + tomorrowMinCelsius + "度です。";
+        String talking = reportTitle + "です。今日の天気";
+        if (toDayCelsius != null) {
+            // 今日の気温が取得できている場合
+            talking += "と気温です。" + toDayAboutReport + "。現在気温" + toDayCelsius + "度。";
+        } else {
+            // 今日の気温が取得できていない場合
+            talking += "です。" + toDayAboutReport + "。";
+        }
+        talking += "明日の天気と気温です。" + tomorrowAboutReport + "。最高気温" + tomorrowMaxCelsius + "度。" + "最低気温" + tomorrowMinCelsius + "度です。";
         // 半角スペースと改行の削除
         talking = talking.replace("\n", "");
         talking = talking.replace("\u0020", "");
